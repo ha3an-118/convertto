93081cc1a74e5e02e20aaa4499099102c77998bf
hassanabedi <habedi11100@gmail.com>
2019-03-28 02:01:37 +0430

    test for menu

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 // register menu one menu for phone one for social menu and one for top menu
 
 
-function register_menu(){
+function ha_register_menu(){
   $arg = array(
       "top-menu" => __("Desktop header menu "),
       "phone-menu" => __("Phone header menu"),
@@ -12,7 +12,18 @@ function register_menu(){
   );
   register_nav_menus($arg);
 }
-add_action('init', 'register_menu');
+add_action('init', 'ha_register_menu');
+
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menus' );
 
 
 
